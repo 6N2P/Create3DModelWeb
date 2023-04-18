9625c9a17d9be89b900eecad76f07e405b4d7573
6N2P <umnik1985@mail.ru>
2023-04-18 09:51:42 +0500

    first commit, test worck with servic

---
+++ Create3DModelWeb/Controllers/MetodController.cs
@@ -0,0 +1,33 @@
+﻿using Create3DModelWeb.Models;
+using Creator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Create3DModelWeb.Controllers
+{
+    public class MetodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> CreateCub(ValuesModel values)
+        {
+            var client = new Creator.CreatorClient(CreatorClient.EndpointConfiguration.BasicHttpBinding_ICreator);
+            ValuesModel v = new ValuesModel();
+            v.height = values.height;
+            v.width = values.width;
+            v.thickness = values.thickness;
+            v.calculation = await client.CreateCubAsync(values.height, values.width, values.thickness);
+
+            ViewBag.Calcul = v.calculation;
+            if (ModelState.IsValid)
+            {
+                return View("Index");
+            }
+
+            client.Close();
+            return View("Index");
+        }
+    }
+}
---
+++ Create3DModelWeb/Models/ValuesModel.cs
@@ -0,0 +1,18 @@
+﻿using Creator;
+using System.ServiceModel;
+
+namespace Create3DModelWeb.Models
+{
+    public class ValuesModel
+    {
+        private ICreator creator;
+
+        public int width { get; set; }
+        public int height { get; set; }
+        public int thickness { get; set; }
+        public int calculation { get; set; } = 0;
+
+      
+    }
+
+}
